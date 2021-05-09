6832ba5a2652a1c24baa27bdb1cdbba1b20204f1
Rodrigo Achilles <rodrigo.achilles@serpro.gov.br>
2021-05-09 08:42:39 -0300

    test: add dos testes e2e
    criação dos testes e2e utilizando o cypress

---
+++ cypress/integration/pages/index.spec.js
@@ -0,0 +1,75 @@
+/// <reference types="cypress" />
+
+import ContactFormPageObject from '../../../src/components/screens/HomeScreen/ContactForm.pageObject';
+
+describe('/pages/index', () => {
+  describe('FormContact', () => {
+    describe('abrindo modal', () => {
+      it('quando clicar no header Contato', () => {
+        const contactForm = new ContactFormPageObject(cy);
+
+        cy.get('div[class*="ModalWrapper"] :first').should('have.attr', 'style')
+          .should('contain', 'transform: translateY(100%) translateZ(0px)');
+
+        contactForm.openModal();
+
+        cy.get('div[class*="ModalWrapper"] :first').should('have.attr', 'style')
+          .should('contain', 'transform: translateY(0%) translateZ(0px)');
+      });
+
+      it('quando clicar no button Contato', () => {
+        const contactForm = new ContactFormPageObject(cy);
+
+        cy.get('div[class*="ModalWrapper"] :first').should('have.attr', 'style')
+          .should('contain', 'transform: translateY(100%) translateZ(0px)');
+
+        contactForm.openModal(false);
+
+        cy.get('div[class*="ModalWrapper"] :first').should('have.attr', 'style')
+          .should('contain', 'transform: translateY(0%) translateZ(0px)');
+      });
+    });
+
+    describe('quando preencher os campos do form de contato', () => {
+      it.skip('e der sucesso', () => {
+        const contactForm = new ContactFormPageObject(cy);
+
+        cy.intercept('https://contact-form-api-jamstack.herokuapp.com/message')
+          .as('submitForm');
+
+        contactForm
+          .openModal()
+          .fillForm({
+            name: 'Rodrigo',
+            email: 'rodrigo.achilles@gmail.com',
+            message: 'Teste do cypress',
+          })
+          .submitForm();
+
+        cy.wait('@submitForm')
+          .then(() => {
+            cy.get('p[id="successMessage"]').should('exist');
+          });
+      });
+      it('e der erro', () => {
+        const contactForm = new ContactFormPageObject(cy);
+
+        cy.intercept('https://contact-form-api-jamstack.herokuapp.com/messag')
+          .as('submitForm');
+
+        contactForm.openModal();
+
+        contactForm.fillForm({
+          name: 'Rodrigo',
+          email: 'rodrigo.achilles@gmail.com',
+          message: 'Teste do cypress',
+        }).submitForm();
+
+        cy.wait('@submitForm')
+          .then(() => {
+            cy.get('p[id="failMessage"]').should('exist');
+          });
+      });
+    });
+  });
+});
